a71fdec9636e5c35f84fa7ddfd368b1b76df04f5
Kaishiyoku <dmp@nfze.de>
2024-03-30 20:30:02 +0100

    adjusted crawler version

--- src/HeraRssCrawler.php
+++ src/HeraRssCrawler.php
@@ -28,7 +28,7 @@ use Symfony\Component\DomCrawler\Crawler;
 
 class HeraRssCrawler
 {
-    private const VERSION = '6.0.0';
+    private const VERSION = '6.0.1';
 
     private ?Client $httpClient;
 
